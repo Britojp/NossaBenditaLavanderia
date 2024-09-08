976a16cad397d2c9be016eec4d72eef504e50f51
guilhermebsc <guilhermebsc2@gmail.com>
2024-09-07 22:47:02 -0300

    Sistema de Avaliação Google

---
+++ script.js
@@ -0,0 +1,13 @@
+let slideIndex = 0;
+
+function mudarSlide(n) {
+    const slides = document.querySelectorAll('.avaliacao');
+    const totalSlides = slides.length;
+    const visibleSlides = 3;
+
+    slideIndex = (slideIndex + n + totalSlides) % totalSlides;
+    const carrossel = document.querySelector('.carrossel');
+    carrossel.style.transform = `translateX(${-slideIndex * (100 / visibleSlides)}%)`;
+}
+
+document.addEventListener('DOMContentLoaded', () => mudarSlide(0));
